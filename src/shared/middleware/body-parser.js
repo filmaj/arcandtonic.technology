adf1b7586f5914d120f91f51b233fb943cd9962d
filmaj <maj.fil@gmail.com>
2019-07-19 08:52:15 -0500

    🔪🤡 lol fuck the type-is module. waste of time.

--- src/shared/middleware/body-parser.js
+++ src/shared/middleware/body-parser.js
@@ -1,35 +1,42 @@
-let typeis = require('type-is');
 let qs = require('querystring');
 
 const JSON_TYPES = ['application/json'];
 const FORM_TYPES = ['application/x-www-form-urlencoded'];
+const WILL_PARSE = JSON_TYPES.concat(FORM_TYPES);
 
 module.exports = function (req) {
-  if (!typeis.hasBody(req) || typeof req.body === 'object') return;
-  let parsed;
+  if (!req.body || typeof req.body === 'object') return;
+  console.log('body parser has a body to work with');
+  let type = req.headers['content-type'];
 
-  if (typeis(req, JSON_TYPES)) {
-    try {
-      parsed = JSON.parse(req.body)
-    } catch (e) {
-      return {
-        statusCode: 400,
-        headers: {'content-type': JSON_TYPES[0]},
-        body: JSON.stringify({error: e.message})
-      };
+  if (WILL_PARSE.includes(type)) {
+    console.log('body parser will parse');
+    let parsed;
+    if (JSON_TYPES.includes(type)) {
+      console.log('body parser will parse json');
+      try {
+        parsed = JSON.parse(req.body)
+      } catch (e) {
+        return {
+          statusCode: 400,
+          headers: {'content-type': JSON_TYPES[0]},
+          body: JSON.stringify({error: e.message})
+        };
+      }
+    } else if (FORM_TYPES.includes(type)) {
+      console.log('body parser will parse form');
+      let b64 = Buffer.from(req.body, 'base64').toString();
+      try {
+        parsed = qs.parse(b64);
+      } catch (e) {
+        return {
+          statusCode: 400,
+          headers: {'content-type': 'text/html'},
+          body: JSON.stringify(e, null, 2)
+        };
+      }
     }
-  } else if (typeis(req, FORM_TYPES)) {
-    let b64 = Buffer.from(req.body, 'base64').toString();
-    try {
-      parsed = qs.parse(b64);
-    } catch (e) {
-      return {
-        statusCode: 400,
-        headers: {'content-type': 'text/html'},
-        body: JSON.stringify(e, null, 2)
-      };
-    }
+    req.body = parsed;
+    return req;
   }
-  req.body = parsed;
-  return req;
 };
